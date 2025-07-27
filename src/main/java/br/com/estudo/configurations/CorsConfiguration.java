706923a84d89f9d221a66fd3acaff2eff0a1b2e1
RobertPorteles <robertporteless@gmail.com>
2025-07-27 00:17:49 -0300

    feat: adição de Cors Configurations

--- src/main/java/br/com/estudo/configurations/CorsConfiguration.java
+++ src/main/java/br/com/estudo/configurations/CorsConfiguration.java
@@ -1,5 +1,24 @@
 package br.com.estudo.configurations;
 
-public class CorsConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class CorsConfiguration {
+	 @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**") // aplica a todos os endpoints
+	                        .allowedOrigins("http://localhost:4200") // origem do seu Angular
+	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	                        .allowedHeaders("*")
+	                        .allowCredentials(true); // se usa cookies, auth, etc
+	            }
+	        };
+	    }
 }
